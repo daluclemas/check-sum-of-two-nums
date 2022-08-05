16bf647dd9475214dec99dbfef80b3facf53b100
daluclemas <59066632+daluclemas@users.noreply.github.com>
2022-08-05 12:04:50 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const checkSum = (arr, target) =>{
+  for(let i = 0; i < arr.length ; i++){
+    for(let j = i + 1 ; j < arr.length ; j++){
+      if(arr[i] + arr[j] === target){
+        return [arr[i], arr[j]];
+      }
+    }
+  }
+}
+
+console.log(checkSum([2,11,7,5], 7))
